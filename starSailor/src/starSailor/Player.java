c74f12d26c1fe9d9b270b55f939a5c7e8f2a6e50
mxw596 <mxw596@cca-lg04-13072.cs.bham.ac.uk>
2015-11-27 11:47:02 +0000

    added to the player class, animations

--- starSailor/src/starSailor/Player.java
+++ starSailor/src/starSailor/Player.java
@@ -1,27 +1,62 @@
 package starSailor;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 public class Player {
 	
-	private Image[] playerImages;
-	private int currentFrame = 0;
+	private BufferedImage spriteSheet;
+	private Image[][] playerImages;
+	private int currentIFrame = 0, currentJFrame = 0;
 	private long time;
 	
-	public Player(){
+	public enum Direction{
+		UP, LEFT, DOWN, RIGHT;
+	}
+	
+	public Player(String path){
+		spriteSheet = (BufferedImage) ResourceLoader.getImage(path);
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 4; j++){
+				playerImages[i][j] = spriteSheet.getSubimage(j * 16, i * 16, 16, 16);
+			}
+		}
 		time = System.currentTimeMillis();
+	}
+	
+	public void setDirection(Direction direction){
+		currentIFrame = 0;
+		switch (direction){
+		case UP:
+			currentJFrame = 0;
+			break;
+		case LEFT:
+			currentJFrame = 1;
+			break;
+		case DOWN:
+			currentJFrame = 2;
+			break;
+		case RIGHT:
+			currentJFrame = 3;
+			break;
+		}
 	}
 	
 	public void update(){
 		long newTime = System.currentTimeMillis();
 		if(newTime >= time + 500){
-			if(currentFrame < playerImages.length){
-				currentFrame++;
+			if(currentIFrame < playerImages.length){
+				currentIFrame++;
 			}else{
-				currentFrame = 0;
+				currentIFrame = 0;
 			}
 			time = newTime;
 		}
+	}
+	
+	public void draw(Graphics g){
+		g.drawImage(playerImages[currentIFrame][currentJFrame], Main.width/2 - 8, Main.height /2 - 8, null);
 	}
 
 }
