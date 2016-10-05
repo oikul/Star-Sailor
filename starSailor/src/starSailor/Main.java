f5f2b929f0296560af9907ae42561baf6b54f2cc
Mike <mike.wd1@ntlworld.com>
2016-10-05 17:26:24 +0100

    some changes

--- starSailor/src/starSailor/Main.java
+++ starSailor/src/starSailor/Main.java
@@ -101,7 +101,7 @@ public class Main extends JFrame {
 		player = new Player("character/character_sprites.png", "spaceship/ship_sprites.png");
 		ship = new ShipInterior();
 		sb = new SpaceBattle();
-		Sound.music.loop();
+		//Sound.music.loop();
 		time = System.currentTimeMillis();
 	}
 	
